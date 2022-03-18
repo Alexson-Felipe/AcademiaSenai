83d3bf3bc0131dc6ae6dc53a40b9b9409e8b61cc
Alexson Felipe <alexfelipesilveira@gmail.com>
2022-03-17 22:00:41 -0300

    Resolução de conflitos

--- src/br/com/senai/academiasenai/entity/Professor.java
+++ src/br/com/senai/academiasenai/entity/Professor.java
@@ -1,35 +1,21 @@
 package br.com.senai.academiasenai.entity;
 
-import java.time.LocalDate;
-
 public class Professor {
 
 	private Integer id;
 	private String nomeCompleto;
-	private LocalDate dataDeCasamento;
 
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNomeCompleto() {
 		return nomeCompleto;
 	}
-
 	public void setNomeCompleto(String nomeCompleto) {
 		this.nomeCompleto = nomeCompleto;
-	}
-
-	public LocalDate getDataDeCasamento() {
-		return dataDeCasamento;
-	}
-
-	public void setDataDeCasamento(LocalDate dataDeCasamento) {
-		this.dataDeCasamento = dataDeCasamento;
 	}
 
 }
