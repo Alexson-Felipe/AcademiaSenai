7b8c8624ad066c03e56024bd30dc037581fa1e0a
Alexson Felipe <alexfelipesilveira@gmail.com>
2022-03-17 21:38:28 -0300

    Adicionado novo atributo

--- src/br/com/senai/academiasenai/entity/Professor.java
+++ src/br/com/senai/academiasenai/entity/Professor.java
@@ -1,23 +1,35 @@
 package br.com.senai.academiasenai.entity;
 
+import java.time.LocalDate;
+
 public class Professor {
 
 	private Integer id;
 	private String nomeCompleto;
-	
+	private LocalDate dataDeAdmissao;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNomeCompleto() {
 		return nomeCompleto;
 	}
+
 	public void setNomeCompleto(String nomeCompleto) {
 		this.nomeCompleto = nomeCompleto;
 	}
-	
-	
-	
+
+	public LocalDate getDataDeAdmissao() {
+		return dataDeAdmissao;
+	}
+
+	public void setDataDeAdmissao(LocalDate dataDeAdmissao) {
+		this.dataDeAdmissao = dataDeAdmissao;
+	}
+
 }
